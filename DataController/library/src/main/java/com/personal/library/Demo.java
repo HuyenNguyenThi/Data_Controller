0d2041e625cf238601918f3bde352690c991d0ae
Vu Le <vule@antking.com.vn>
2016-05-12 04:10:27 +0700

    2016-05-12 VULE: DONE - upload aar

---
+++ DataController/library/src/main/java/com/personal/library/Demo.java
@@ -0,0 +1,17 @@
+package com.personal.library;
+
+import android.util.Log;
+
+/**
+ * Created by vubuile on 5/12/16.
+ */
+public class Demo {
+
+    public Demo() {
+
+    }
+
+    public void show() {
+        Log.e("DATA CONTROLLER", "=> SHOW DEMO");
+    }
+}
